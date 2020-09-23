10158d8173460139755b8c22a85fc8a00bf97bd1
hungtran <hung.tran@bbv.vn>
2020-09-23 09:46:17 +0700

    commit changes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,8 +1,17 @@
+const CACHE_NAME = 'gih-cache';
+const CACHED_URLS = [
+  '/index-offline.html',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+  '/css/gih-offline.css',
+  '/img/jumbo-background-sm.jpg',
+  '/img/logo-header.png'
+];
+
 // 'install' event called after registering the service worker
 self.addEventListener('install', (event) => {
   // wait until successfully caching the file before declaring SW installation a success & activating new SW
   event.waitUntil(
-    caches.open('gih-cache').then((cache) => cache.add('/index-offline.html'))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHED_URLS))
   );
 });
 
@@ -10,6 +19,16 @@ self.addEventListener('fetch', (event) => {
   // The match is not verified since it must exist with SW
   event.respondWith(
     fetch(event.request)
-      .catch(() => caches.match('/index-offline.html'))
+      .catch(() => {
+        return caches.match(event.request)
+          .then((response) => {
+            if (response) {
+              return response;
+            } else if (event.request.headers.get('accept').includes('text/html')) {
+              // the browser never explicitly asks for 'index-offline.html', hence the check here
+              return caches.match('/index-offline.html');
+            }
+          });
+      })
   );
 });
