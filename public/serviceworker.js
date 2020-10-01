e965b82a11608b348e7a39bc61b92f070f926fe8
hungtran <hung.tran@bbv.vn>
2020-10-01 09:53:32 +0700

    commit changes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = 'gih-cache-v4';
-var CACHED_URLS = [
+const CACHE_NAME = 'gih-cache-v4';
+const CACHED_URLS = [
   '/index-offline.html',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
   '/css/gih-offline.css',
@@ -7,11 +7,35 @@ var CACHED_URLS = [
   '/img/logo-header.png',
 ];
 
+const immutableRequests = [
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+  '/img/jumbo-background-sm.jpg',
+  '/img/logo-header.png',
+];
+
+const mutableRequests = ['/index-offline.html', '/css/gih-offline.css'];
+
 // triggered after SW is registered
 self.addEventListener('install', function (event) {
   // check 'The Service Worker Lifetime...' on page 73 for the need for 'waitUntil'
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHED_URLS))
+    caches.open(CACHE_NAME).then((cache) => {
+      const newImmutableRequests = [];
+
+      return Promise.all(
+        immutableRequests.map((url) => {
+          return caches.match(url).then((response) => {
+            if (response) {
+              return cache.put(url, response);
+            } else {
+              newImmutableRequests.push(url);
+
+              return Promise.resolve();
+            }
+          });
+        })
+      ).then(() => cache.addAll(newImmutableRequests.concat(mutableRequests)));
+    })
   );
 });
 
