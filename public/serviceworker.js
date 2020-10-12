57413d638e6e0d0f3fc2ad2bb604f756173631bf
hungtran <hung.tran@bbv.vn>
2020-10-12 09:42:06 +0700

    commit changes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,40 +1,75 @@
-var CACHE_NAME = "gih-cache-v3";
+var CACHE_NAME = 'gih-cache-v4';
 var CACHED_URLS = [
-  "/index-offline.html",
-  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
-  "/css/gih-offline.css",
-  "/img/jumbo-background-sm.jpg",
-  "/img/logo-header.png"
+  // HTML
+  '/index.html',
+  // Stylesheets
+  '/css/gih.css',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css?family=Lato:300,600,900',
+  // Javascript
+  'https://code.jquery.com/jquery-3.0.0.min.js',
+  '/js/app.js',
+  // Images
+  '/img/logo.png',
+  '/img/logo-header.png',
+  '/img/event-calendar-link.jpg',
+  '/img/switch.png',
+  '/img/logo-top-background.png',
+  '/img/jumbo-background.jpg',
+  '/img/reservation-gih.jpg',
+  '/img/about-hotel-spa.jpg',
+  '/img/about-hotel-luxury.jpg',
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(CACHED_URLS);
     })
   );
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index-offline.html");
-        }
-      });
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  const requestURL = new URL(event.request.url);
+
+  // cache, falling back to network w frequent updates
+  if (requestURL.pathname === '/' || requestURL.pathname === '/index.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match('/index.html').then((cachedResponse) => {
+          // fetch & cache the latest version whether ore not index.html is found
+          const fetchPromise = fetch('/index.html').then((networkResponse) => {
+            cache.put('/index.html', networkResponse.clone());
+
+            return networkResponse;
+          });
+
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)
+  ) {
+    event.respondWith(
+      caches
+        .open(CACHE_NAME)
+        .then((cache) =>
+          cache
+            .match(event.request)
+            .then((response) => response || fetch(event.request))
+        )
+    );
+  }
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
+        cacheNames.map(function (cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith('gih-cache')) {
             return caches.delete(cacheName);
           }
         })
