701fec4fd0f7e689d141e4bf1e2e43c84a2d744d
hungtran <hung.tran@bbv.vn>
2020-11-12 09:43:17 +0700

    commit changes

--- public/js/my-account.js
+++ public/js/my-account.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
 // Fetches reservations from server and renders them to the page
 var populateReservations = function () {
-  getReservations(renderReservations);
+  getReservations().then((reservations) => renderReservations(reservations));
 };
 
 // Go over unconfirmed reservations, and verify their status against the server.
